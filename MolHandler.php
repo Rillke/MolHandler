8ead38837e6c1352463aabe5d02b0036dcd719d4
Rainer Rillke <rillke@wikipedia.de>
2014-04-08 12:45:48 +0200

    Create MolHandler.php

---
+++ MolHandler.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * MolHandler extension
+ *
+ * @file
+ * @ingroup Extensions
+ *
+ * Stores the setup instructions for MolHandler.
+ *
+ * Usage: Include the following line in your LocalSettings.php
+ * require_once( "$IP/extensions/MolHandler/MolHandler.php" );
+ *
+ * @author Rainer Rillke <[lastname]@wikipedia.de>
+ * @license GPL v2 or later
+ * @version 0.1
+ */
+
+/* Configuration */
+
+// Credits
+$wgExtensionCredits['other'][] = array(
+	'path' => __FILE__,
+	'name' => 'Mol Handler',
+	'author' => array(
+		'Rainer Rillke'
+	),
+	'version' => '0.1.0',
+	'descriptionmsg' => 'molhandler-desc',
+	'url' => 'https://www.mediawiki.org/wiki/Extension:MolHandler'
+);
+
