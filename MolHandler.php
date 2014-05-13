48297358a8e1f98b61bb44f38936fb581f06f6d9
rillke <rainerrillke@hotmail.com>
2014-05-13 14:46:58 +0200

    Finishing skeleton

--- MolHandler.php
+++ MolHandler.php
@@ -9,32 +9,38 @@
  *
  * Usage: Include the following line in your LocalSettings.php
  * require_once( "$IP/extensions/MolHandler/MolHandler.php" );
+ * The indigo-depict tool must be present.
  *
  * @author Rainer Rillke <[lastname]@wikipedia.de>
- * @license GPL v2 or later
+ * @license GPL.v2 or later
  * @version 0.1
  */
+
+# Not a valid entry point, skip unless MEDIAWIKI is defined
+if ( !defined( 'MEDIAWIKI' ) ) {
+	echo 'MolHandler is a MediaWiki extension.';
+	exit( 1 );
+}
 
 /* Configuration */
 
 // Credits
-$wgExtensionCredits['other'][] = array(
+$wgExtensionCredits['media'][] = array(
 	'path' => __FILE__,
 	'name' => 'Mol Handler',
 	'author' => array(
-		'Rainer Rillke'
+		'Rainer Rillke',
 	),
 	'version' => '0.1.0',
-	'descriptionmsg' => 'molhandler-desc',
-	'url' => 'https://www.mediawiki.org/wiki/Extension:MolHandler'
+	'descriptionmsg' => 'mwe-mh-credits-desc',
+	'url' => 'https://www.mediawiki.org/wiki/Extension:MolHandler',
+	'license-name' => 'GPL-2.0+',
 );
 
 $wgMolHandlerDir = __DIR__;
+$wgMessagesDirs['MolHandler'] = $wgMolHandlerDir . '/i18n';
 
 # Require modules
 $wgAutoloadClasses += array(
-	// Hooks
-	// 'MolHandlerHooks' => $wgMolHandlerDir . '/MolHandlerHooks.php',
+	// Hooks ...
 );
-
-// $wgHooks['UnitTestsList'][] = 'MolHandlerHooks::onUnitTestsList';
